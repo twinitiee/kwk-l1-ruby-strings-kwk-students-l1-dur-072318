2778b6a45835238e57fb089ffe732d02cd9184b8
twinitiee <twinitiee@gmail.com>
2018-07-28 13:21:12 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,24 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+
+puts "Welcome!"
+puts "Please insert your information for your party."
+puts "Please insert guest name."
+guest_name = gets.strip
+puts "Please insert party name."
+party_name = gets.strip
+puts "Please insert date."
+date = gets.strip
+puts "Please insert time."
+time = gets.strip
+puts "Please insert host name."
+host_name = gets.strip
+
+puts "Dear #{guest_name}
+
+You are cordially invited to #{party_name} on #{date} at #{time}.
+
+Sincerly,
+
+#{host_name}"
